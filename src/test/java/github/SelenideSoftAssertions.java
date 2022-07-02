448e5770e84174d3f1bb451f86cf8198a512e0ac
EmelyanovPV <emelyanovpv@bcs.ru>
2022-07-02 12:38:10 +0700

    Правки по замечанию к ДЗ

--- src/test/java/github/SelenideSoftAssertions.java
+++ src/test/java/github/SelenideSoftAssertions.java
@@ -3,6 +3,7 @@ package github;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SelenideSoftAssertions {
@@ -12,7 +13,10 @@ public class SelenideSoftAssertions {
 
         open("https://github.com/selenide/selenide");
         $("[id=wiki-tab]").click();
-        $("[id=wiki-body]").$$("a").filterBy(text("Soft assertions")).first().click();
+        $(byTagAndText("button", "Show 2 more pages…")).click();
+        $(byTagAndText("a", "Soft assertions")).click();
         $("[id=user-content-3-using-junit5-extend-test-class]").shouldBe(visible);
+
+        sleep(5000);
     }
 }
